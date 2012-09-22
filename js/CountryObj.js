adf61892deb45939a06398c560ead04815f2edd4
Juvoni Beckford <jksbec@gmail.com>
2012-09-22 19:57:59 -0400

    Fixed err_y

--- js/CountryObj.js
+++ js/CountryObj.js
@@ -38,8 +38,9 @@ function CountryObj(name, code, rating){
 
 
 };
-function err_y(){
-	console.log("Invalid Year");
+function err_y(year){
+	if(year<1999)
+		console.log("Invalid Year");
 };
 CountryObj.prototype ={
 	getName: function(){
@@ -83,53 +84,43 @@ CountryObj.prototype ={
 		this.Economic['Unemployment_rate'][key] = val;
 	},
 	getNomGDP: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Nominal_GDP'][year];
 	},
 	getPerCapita: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Per_capita_GDP'][year];
 	},
 	getGross_d_Saving: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Gross_domestic_savings'][year];
 	},
 	getGross_d_Investment: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Gross_domestic_investment'][year];
 	},
 	getReal_GDP_G: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Real_GDP_growth'][year];
 	},
 	getRealInvestment: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Real_investment'][year];
 	},
 	getCPI: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Consumer_price_index'][year];
 	},
 	getNFPE: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Oth_DC_claims_on_private_NFPEs'][year];
 	},
 	getBank_Claim_res: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Bank_claims_on_resident_non_govt_sectors'][year];
 	},
 	getUN: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Economic['Unemployment_rate'][year];
 	},
 
@@ -159,43 +150,35 @@ CountryObj.prototype ={
 		this.Fiscal['Interest_To_GDP'][key] = val;
 	},
 	getDebtToGDP: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Fiscal['Net_Gov_debt_per_GDP'][year];
 	},
 	getDepositToGDP: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Fiscal['GG_debtDeposits_GDP'][year];
 	},
 	getGov_Debt: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Fiscal['Gov_Debt'][year];
 	},
 	getGov_Bal: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Fiscal['Gov_balance'][year];
 	},
 	getGov_P_Bal: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Fiscal['Gov_Pri_Bal'][year];
 	},
 	getGov_Rev: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Fiscal['Revenues'][year];
 	},
 	getGov_Ex: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Fiscal['Expenditures'][year];
 	},
 	getInterestToGDP: function(year){
-		if(year<1999)
-			err_y();
+			err_y(year);
 		return this.Fiscal['Interest_To_GDP'][year];
 	}
 
@@ -203,10 +186,3 @@ CountryObj.prototype ={
 
 };
 
-Object.size = function(obj) {
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
-};
