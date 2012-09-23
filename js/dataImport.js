c331707dcf470c879d4838efaee539fd7fda1080
Juvoni Beckford <jksbec@gmail.com>
2012-09-22 20:42:46 -0400

    Added External/IIP Getters/Setters & Data Import

--- js/dataImport.js
+++ js/dataImport.js
@@ -48,4 +48,43 @@ $(document).ready(function() {
 		});
 	});
 
+	$.getJSON('ajax/fiscal_data.json', function(data) {
+		var n = 0; //Country counter
+		$.each(data, function(){
+			for(var  i = startYear; i<=endYear; i++){
+					EU[n].addDebtToGDP(i,this['Data']['Net general government debt as % of GDP (%)'][i]),
+					EU[n].addDepositToGDP(i,this['Data']['GG debt net of deposits / GDP (%)'][i]),
+					EU[n].addGov_Debt(i,this['Data']['General government debt'][i]),
+					EU[n].addGov_Bal(i,this['Data']['General government balance'][i]),
+					EU[n].addGov_P_Bal(i,this['Data']['General government balance'][i]),
+					EU[n].addGov_Rev(i,this['Data']['General government revenues'][i]),
+					EU[n].addGov_Ex(i,this['Data']['General government expenditures'][i]),
+					EU[n].addInterestToGDP(i,this['Data']['GG interest paid / GDP (%)'][i])
+				}
+				n++;
+		});
+	});
+
+	$.getJSON('ajax/external.json', function(data) {
+		var n = 0; //Country counter
+		$.each(data, function(){
+			for(var  i = startYear; i<=endYear; i++){
+					EU[n].addELiabilities(i,this['Data']['Net external liabilities'][i]),
+					EU[n].addEDebt(i,this['Data']['Gross external debt'][i]),
+					EU[n].addPublicToCar(i,this['Data']['Public sector ext. debt / CAR (%)'][i]),
+					EU[n].addExternalToCar(i,this['Data']['Net external debt / CAR (%)'][i]),
+					EU[n].addNarrowExToCar(i,this['Data']['Narrow net external debt (% of CARs)'][i]),
+					EU[n].addPublicExternal(i,this['Data']['Net public sector external debt'][i]),
+					EU[n].addInvestmentPay(i,this['Data']['Net investment payments'][i]),
+					EU[n].addInterestPay(i,this['Data']['Net interest payments'][i])
+				}
+				n++;
+		});
+		var e = 14;
+		console.log(EU[e].getName());
+		for(var i = 1999; i<=2012;i++)
+			console.log(i+" "+EU[e].getInterestPay(i));
+	});
+
 });
+
